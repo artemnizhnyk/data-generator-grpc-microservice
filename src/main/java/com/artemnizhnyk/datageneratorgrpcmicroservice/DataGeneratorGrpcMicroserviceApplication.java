5c5b5f3f5052e72f1645952368affa032cc9b8cc
Artem Nizhnyk <nizhnyk4253@gmail.com>
2024-04-02 18:49:13 +0200

    Initial commit

---
+++ src/main/java/com/artemnizhnyk/datageneratorgrpcmicroservice/DataGeneratorGrpcMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.datageneratorgrpcmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataGeneratorGrpcMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataGeneratorGrpcMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artemnizhnyk/datageneratorgrpcmicroservice/DataGeneratorGrpcMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemnizhnyk.datageneratorgrpcmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataGeneratorGrpcMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
